fd8591ab70f482459843b691c073ba2e23f8a1a9
HiddenGarage <thehidngarage@gmail.com>
2026-04-20 18:12:33 -0400

    Delete TurboPunch - SITE directory

--- TurboPunch - SITE/netlify/functions/auth-logout.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function handler() {
-  return {
-    statusCode: 302,
-    headers: {
-      Location: "/",
-      "Set-Cookie": "tunerclock_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0"
-    }
-  };
-};

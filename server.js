25b62f378033e5d5a5da7c638682c296de7a9a85
HiddenGarage <thehidngarage@gmail.com>
2026-04-22 07:14:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ const PAYSLIP_SIGNATURE = "Signé Léo Belleamy et Niko Walker | Santos Tuners I
 const ROLE_DEFINITIONS = [
   { name: "Patron", id: "1487868408228741171", hourlyRate: 60, isAdmin: true },
   { name: "Copatron", id: "1487666934412611594", hourlyRate: 45, isAdmin: true },
-  { name: "Gerant", id: "1487852908077781168", hourlyRate: 35, isAdmin: true },
-  { name: "Gouvernement", id: "1494749026694987816", hourlyRate: 0, isAdmin: true, canManage: false },
+  { name: "Gerant", id: "1487852908077781168", hourlyRate: 35, isAdmin: true, canManage: false },
+  { name: "Gouvernement", id: "1494749026694987816", hourlyRate: 0, isAdmin: true, canManage: false, isSupervision: true },
   { name: "Mecano", id: "1487852832643354665", hourlyRate: 25, isAdmin: false },
   { name: "Apprenti", id: "1487852702519136496", hourlyRate: 18, isAdmin: false }
 ];
@@ -982,6 +982,7 @@ app.get("/auth/discord/callback", async (req, res) => {
       avatar: profile.avatar,
       isAdmin,
       canManage,
+      isSupervision: roleName === "Gouvernement",
       readOnly: isAdmin && canManage === false
     };
 

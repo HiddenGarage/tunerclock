949620ccdf73f6da13e2821dd466361337ea037e
HG-Scripts <thehidngarage@gmail.com>
2026-04-26 09:59:23 -0400

    Update auth-logout.js

--- netlify/functions/auth-logout.js
+++ netlify/functions/auth-logout.js
@@ -1,9 +1,11 @@
+const { buildCookie } = require("./lib/session");
+
 exports.handler = async function handler() {
   return {
     statusCode: 302,
     headers: {
       Location: "/",
-      "Set-Cookie": "tunerclock_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0"
+      "Set-Cookie": buildCookie("tunershub_session", "", 0)
     }
   };
 };

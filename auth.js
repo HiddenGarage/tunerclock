b9b38de420855b66da22161fe270db2e5c46a5a3
HG-Scripts <thehidngarage@gmail.com>
2026-04-26 06:39:32 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,39 +1,62 @@
-const { required } = require("../netlify/functions/lib/env");
+const { required } = require("./netlify/functions/lib/env");
 const {
   parseCookies,
   decodeSession,
-} = require("../netlify/functions/lib/session");
+} = require("./netlify/functions/lib/session");
+const {
+  refreshDiscordSession,
+  writeSessionCookie,
+} = require("./discord-session");
 
 function getSession(req) {
   const cookies = parseCookies(req.headers.cookie || "");
   return decodeSession(cookies.tunershub_session, required("SESSION_SECRET"));
 }
 
-function requireAuth(req, res, next) {
+async function resolveRequestSession(req, res) {
   const session = getSession(req);
+  if (!session) return null;
+
+  const refreshedSession = await refreshDiscordSession(session).catch(
+    () => session,
+  );
+  req.session = refreshedSession;
+
+  if (JSON.stringify(refreshedSession) !== JSON.stringify(session)) {
+    writeSessionCookie(res, refreshedSession);
+  }
+
+  return refreshedSession;
+}
+
+async function requireAuth(req, res, next) {
+  const session = await resolveRequestSession(req, res);
   if (!session) {
     return res.status(401).send("Non autorise.");
   }
-  req.session = session;
   next();
 }
 
-function requireAdminAccess(req, res, next) {
-  const session = getSession(req);
+async function requireAdminAccess(req, res, next) {
+  const session = await resolveRequestSession(req, res);
   if (!session || !session.isAdmin) {
     return res.status(403).send("Acces refuse.");
   }
-  req.session = session;
   next();
 }
 
-function requireAdmin(req, res, next) {
-  const session = getSession(req);
+async function requireAdmin(req, res, next) {
+  const session = await resolveRequestSession(req, res);
   if (!session || !session.isAdmin || session.canManage === false) {
     return res.status(403).send("Acces lecture seule.");
   }
-  req.session = session;
   next();
 }
 
-module.exports = { getSession, requireAuth, requireAdminAccess, requireAdmin };
+module.exports = {
+  getSession,
+  resolveRequestSession,
+  requireAuth,
+  requireAdminAccess,
+  requireAdmin,
+};
